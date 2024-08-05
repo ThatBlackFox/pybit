0b231b01118d3381c31a4a91f0b71ab30f6f8d70
ThatBlackFox <108296426+ThatBlackFox@users.noreply.github.com>
2024-08-05 15:12:23 +0530

    Doing stage 3

--- app/main.py
+++ app/main.py
@@ -1,15 +1,28 @@
 import json
 import sys
 
+
+def handle_int(bencoded_value:bytes):
+    e_index = bencoded_value.find(b"e")
+    return int(bencoded_value[1:e_index])
+
+def handle_str(bencoded_value:bytes):
+    first_colon_index = bencoded_value.find(b":")
+    if first_colon_index == -1:
+        raise ValueError("Invalid encoded value")
+    return bencoded_value[first_colon_index+1:]
+
+def handle_list(bencoded_value:bytes):
+    #TODO: will handle lists after lunch #SBI
+    pass
+
 def decode_bencode(bencoded_value:bytes):
     if chr(bencoded_value[0]).isdigit():
-        first_colon_index = bencoded_value.find(b":")
-        if first_colon_index == -1:
-            raise ValueError("Invalid encoded value")
-        return bencoded_value[first_colon_index+1:]
+        return handle_str(bencoded_value)
     elif chr(bencoded_value[0])=='i':
-        e_index = bencoded_value.find(b"e")
-        return int(bencoded_value[1:e_index])
+        return handle_int(bencoded_value)
+    elif chr(bencoded_value[0])=='l':
+        return handle_list(bencoded_value)
     else:
         raise NotImplementedError("Only strings are supported at the moment")
 
